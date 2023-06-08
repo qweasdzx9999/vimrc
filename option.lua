843876822af208190eb96585f2b10a88b7de067b
qweasdzx9999 <95947963+qweasdzx9999@users.noreply.github.com>
2023-06-08 17:20:15 +0800

    Add files via upload

---
+++ option.lua
@@ -0,0 +1,23 @@
+vim.o.number = true
+vim.o.guifont = "Fira Code + WFM Sans SC:h18"
+
+vim.o.encoding = 'utf-8'
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.tabstop= 4
+vim.o.shiftwidth= 4
+vim.o.number = true
+vim.o.signcolumn= "yes"
+
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+vim.o.history= 1000
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.showmode = false
+
+vim.o.wrap = true
